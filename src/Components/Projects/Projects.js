ec46e745833f750ad567afdf9b63d5540901eb03
John LaTona <johnlatona@Johns-MBP.fios-router.home>
2018-03-15 11:51:03 -0400

    update

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const ProjectContent = (props) => {
+    const { link, img, alt, description, dataFor, ...rest} = props;
+    console.log(...rest);
+    return (
+    <a {...rest} data-tip data-for={props.dataFor} href={props.link} target="_blank" rel="noopener noreferrer" className="projectDetails">
+        <img src={props.img} alt={props.alt} width="100%"/>
+        <div className="projectDescription">
+            <p>{props.description}</p>
+        </div>
+    </a> 
+)};
+
+export const ProjectLink = props => {
+    const { link, name, dataFor, ...rest } = props;
+    return (
+    <a {...rest} data-tip data-for={props.dataFor} href={props.link} target="_blank" rel="noopener noreferrer">
+        <h3 className="projectTitles">{props.name}</h3>
+    </a>
+)};
---
+++ src/Components/Projects/index.js
@@ -0,0 +1,1 @@
+export { ProjectContent, ProjectLink } from "./Projects";
